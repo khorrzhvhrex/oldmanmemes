9eb0211e487ac0f272d99c8844a7b22e724a8c36
khorrzh_vhrex <khorrzhvhrex@gmail.com>
2026-06-02 15:58:46 -0500

    initial app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+document.getElementById("prevBtn")
+    .addEventListener("click", () => {
+        alert("Previous Meme");
+    });
 
+document.getElementById("nextBtn")
+    .addEventListener("click", () => {
+        alert("Next Meme");
+    });
+
+document.getElementById("randomBtn")
+    .addEventListener("click", () => {
+        alert("Random Meme");
+    });
